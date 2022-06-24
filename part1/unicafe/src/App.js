c681f042dda1781e65fd6b735452c16737f8c67c
Paul Suh <phs92@cornell.edu>
2022-06-24 12:13:26 -0700

    finish unicafe

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -6,11 +6,36 @@ const Button = ({onClick, text}) => (
   </button>
 )
 
-const Display = ({state, text}) => (
-  <p>
-    {text} {state}
-  </p>
+const StatisticLine = ({label, content}) => (
+  <tr>
+    <td> {label} </td> 
+    <td> {content} </td>
+  </tr>
 )
+
+const Statistics = ({good, neutral, bad}) => {
+  const total = good + neutral + bad
+  let body;
+  if (good === 0 && neutral === 0 && bad===0){
+    body = <p>No feedback given</p>
+  }
+  else {
+    body =<table>
+      <tbody>
+        <StatisticLine content={good} label="Good:"/>
+        <StatisticLine content={neutral} label="Neutral:"/>
+        <StatisticLine content={bad} label = "Bad:"/>
+        <StatisticLine label ="Average:" content={(total) / 3}/>
+        <StatisticLine label="Total:" content={total}/>
+        <StatisticLine label="Positive:" content={total ? good/total : "Not available"}/>
+      </tbody>
+    </table>
+  }
+  return <div>
+    <h2>Statistics</h2>
+    {body}
+  </div>
+}
 
 const App = () => {
 
@@ -26,16 +51,16 @@ const App = () => {
   const addNeutral = addOne(setNeutral, neutral)
   const addBad = addOne(setBad, bad)
 
+  const total = good+neutral+bad
+
   return (
     <div>
       <h1>Please leave feedback!</h1>
       <Button onClick={addGood} text="Good"/>
       <Button onClick = {addNeutral} text="Neutral"/>
       <Button onClick = {addBad} text="Bad"/>
-      <h2>Statistics</h2>
-      <Display state={good} text="Good:"/>
-      <Display state={neutral} text="Neutral:"/>
-      <Display state={bad} text = "Bad:"/>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+      
     </div>
   )
 }
