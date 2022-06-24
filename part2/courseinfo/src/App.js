8be88a38adcf4408c43b8e54f8f6094f4ca57b6c
Paul Suh <phs92@cornell.edu>
2022-06-24 16:22:02 -0700

    2.5

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,29 +1,4 @@
-const Header = ({name}) => <h2>{name}</h2>
-
-const Part = ({name, exercises}) => {
-  return <p> {name} {exercises} </p>
-}
-
-const Content = ({parts}) => {
-  return (<>
-    {parts.map( (part) => <Part key = {part.id} name={part.name} exercises={part.exercises}/>) }
-  </>)
-}
-
-const Total = ({parts}) => {
-  const total = parts.reduce((accum,part) => accum+part.exercises, 0)
-  return (<p>
-    <strong>total of {total} exercises</strong>
-  </p>)
-}
-
-const Course = ({name, parts}) => {
-  return <>
-    <Header name = {name}/>
-    <Content parts = {parts}/>
-    <Total parts = {parts}/>
-  </>
-}
+import Course from './Course'
 
 const App = () => {
   const courses = [
