2eb0b5895a503a3ead0481773791aeb843fcaad3
Paul Suh <phs92@cornell.edu>
2022-06-24 18:31:49 -0700

    2.6-2.9

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,84 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+  const [newName, setNewName] = useState('')
+
+  const [newNumber, setNewNumber] = useState('')
+
+  const [newSearch, setNewSearch] = useState('')
+
+  const nameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const personExists = () => (
+    persons.find( (person) => person.name === newName)
+  )
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    if (personExists()) {
+      alert(`${newName} is already added to phonebook.`)
+    }
+    else {
+      const newPerson = {
+        name: newName,
+        number: newNumber
+      }
+      setPersons(persons.concat(newPerson))
+      setNewName('')
+      setNewNumber('')
+    }
+  }
+
+  const numberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const displayNumbers = () => {
+    const filteredPersons = persons.filter( (person) => {
+      let name = person.name.toLowerCase()
+      let search = newSearch.toLowerCase()
+      return name.includes(search)
+    })
+    return filteredPersons.map( (person) => (
+    <div key={person.name}>
+      {person.name} {person.number}
+    </div>)
+  )}
+
+  const searchChange = (event) => {
+    setNewSearch(event.target.value)
+  }
+
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <div>
+        filter by: <input value={newSearch} onChange={searchChange}/>
+      </div>
+      <h2>Add new person</h2>
+      <form  onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={nameChange}/>
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={numberChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {displayNumbers()}
+    </div>
+  )
+}
+
+export default App
